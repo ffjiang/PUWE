91fd5cb27ecaf5238db166717a8f6e7dbc2de7b7
Frank Jiang <ffjiang@princeton.edu>
2014-06-30 01:19:29 -0400

    working on home screen

--- scripts/index.js
+++ scripts/index.js
@@ -21,7 +21,7 @@
 (function($) {
 	// Init Skrollr
 	var s = skrollr.init({
-		forceHeight: true,
+		forceHeight: true, // Set to false to eliminate excess whitespace
 		render: function(data) {
 			// Debugging - Log the current scroll position.
 			// console.log(data.curTop);
